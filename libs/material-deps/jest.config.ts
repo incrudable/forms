ed13d6d488854dfbbb825ed26045210cd1419c71
Paul Spears <dpsthree@gmail.com>
2022-05-11 10:00:26 -0500

    update nx

--- libs/material-deps/jest.config.ts
+++ libs/material-deps/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
-  displayName: 'renderers-material-form-renderer',
-  preset: '../../../jest.preset.js',
+  displayName: 'material-deps',
+
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../coverage/libs/renderers/material-form-renderer',
+  coverageDirectory: '../../coverage/libs/material-deps',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
@@ -18,4 +18,5 @@ module.exports = {
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
+  preset: '../../jest.preset.ts',
 };
--- libs/renderers/material-form-renderer/jest.config.ts
+++ libs/renderers/material-form-renderer/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
-  displayName: 'material-deps',
-  preset: '../../jest.preset.js',
+  displayName: 'renderers-material-form-renderer',
+
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/libs/material-deps',
+  coverageDirectory: '../../../coverage/libs/renderers/material-form-renderer',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
@@ -18,4 +18,5 @@ module.exports = {
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
+  preset: '../../../jest.preset.ts',
 };
